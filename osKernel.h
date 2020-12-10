43ef0b0669162ffc8e44586ee024b9ce72a1a60f
dineshkumar51 <64628450+dineshkumar51@users.noreply.github.com>
2020-12-10 20:27:11 +0530

    included priority schedular

--- osKernel.h
+++ osKernel.h
@@ -15,6 +15,22 @@ void task2(void);
 #define PERIOD  100
 void task3(void);
 
+void osSemaphoreInit(int32_t *semaphore, int32_t value);
+void osSignalset(int *semaphore);
+void osSignalwait(int32_t *semaphore);
+
+
+
+void osMailboxInit(void);
+void osMailboxSend(uint32_t data);
+uint32_t osMailboxReceive(void);
+
+void osFifoInit(void);
+int8_t osFifoPut(uint32_t data);
+uint32_t osFifoGet(void);
+
+void osThreadSleep(uint32_t sleep_ticks);
+
 
 
 
@@ -25,9 +41,10 @@ void task3(void);
 
 void osThreadYield(void);
 
-uint8_t osKernelAddThreads(void(*task0)(void),
-						   void(*task1)(void),
-						   void(*task2)(void));
+uint8_t osKernelAddThreads(void(*task0)(void),uint32_t p0,
+	                         void(*task1)(void),uint32_t p1,
+		                       void(*task2)(void),uint32_t p2,
+			                     void(*task3)(void),uint32_t p3);
 						   
 void choose_next(void); 
 
